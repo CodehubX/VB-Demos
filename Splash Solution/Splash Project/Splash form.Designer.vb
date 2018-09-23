1ccf3fedab258fec53b5506455ed5338fbd3e910
Karuzzio <kevinkarash@gmail.com>
2018-09-23 18:49:15 +0800

    CH1-Lesson A & B

--- Splash Solution/Splash Project/Splash form.Designer.vb
+++ Splash Solution/Splash Project/Splash form.Designer.vb
@@ -22,19 +22,74 @@ Partial Class frmSplash
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblMsg = New System.Windows.Forms.Label()
+        Me.picIguanas = New System.Windows.Forms.PictureBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.picIguanas, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(118, 132)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(149, 28)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "WALAWA ZOO"
+        '
+        'lblMsg
+        '
+        Me.lblMsg.AutoSize = True
+        Me.lblMsg.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMsg.Location = New System.Drawing.Point(82, 155)
+        Me.lblMsg.Name = "lblMsg"
+        Me.lblMsg.Size = New System.Drawing.Size(220, 28)
+        Me.lblMsg.TabIndex = 1
+        Me.lblMsg.Text = "THIS IS HOW WE ZOO"
+        '
+        'picIguanas
+        '
+        Me.picIguanas.Image = Global.Splash_Project.My.Resources.Resources.Iguanas
+        Me.picIguanas.Location = New System.Drawing.Point(34, 23)
+        Me.picIguanas.Name = "picIguanas"
+        Me.picIguanas.Size = New System.Drawing.Size(316, 255)
+        Me.picIguanas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picIguanas.TabIndex = 2
+        Me.picIguanas.TabStop = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(298, 275)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(52, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
         '
         'frmSplash
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
         Me.ClientSize = New System.Drawing.Size(389, 301)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblMsg)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.picIguanas)
         Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Name = "frmSplash"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Walawa Zoo"
+        CType(Me.picIguanas, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblMsg As Label
+    Friend WithEvents picIguanas As PictureBox
+    Friend WithEvents btnExit As Button
 End Class
--- Splash Solution/Splash Project/Splash form.vb
+++ Splash Solution/Splash Project/Splash form.vb
@@ -1,3 +1,9 @@
 ﻿Public Class frmSplash
+    Private Sub frmSplash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
 End Class
