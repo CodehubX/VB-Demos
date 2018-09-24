d334baf8e83e21c16423c862a89cb4a74e4ece14
Karuzzio <kevinkarash@gmail.com>
2018-09-24 14:05:48 +0800

    Changed intNumber to Variable

---
+++ HelloUser/Variables/FloatingPointMath.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FloatingPointMath
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnFloatMath = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnFloatMath
+        '
+        Me.btnFloatMath.Location = New System.Drawing.Point(211, 289)
+        Me.btnFloatMath.Name = "btnFloatMath"
+        Me.btnFloatMath.Size = New System.Drawing.Size(75, 23)
+        Me.btnFloatMath.TabIndex = 0
+        Me.btnFloatMath.Text = "Double Test"
+        Me.btnFloatMath.UseVisualStyleBackColor = True
+        '
+        'FloatingPointMath
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(632, 452)
+        Me.Controls.Add(Me.btnFloatMath)
+        Me.Name = "FloatingPointMath"
+        Me.Text = "FloatMath"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnFloatMath As Button
+End Class
---
+++ HelloUser/Variables/FloatingPointMath.vb
@@ -0,0 +1,16 @@
+﻿Public Class FloatingPointMath
+    Private Sub btnFloatMath_Click(sender As Object, e As EventArgs) Handles btnFloatMath.Click
+        'Declare variable
+        Dim dblNumber As Double
+
+        'Set number, multiply numbers, and display results
+        dblNumber = 45.34
+        dblNumber *= 4.333
+        MessageBox.Show(dblNumber.ToString, "Floating Points")
+
+        'set number, divide numbers, and display results
+        dblNumber = 12
+        dblNumber /= 7
+        MessageBox.Show(dblNumber.ToString, "Floating Points")
+    End Sub
+End Class
