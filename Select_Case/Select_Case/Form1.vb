506dc661142bcbb15a94f14899d3897f3b44b7bc
Karuzzio <kevinkarash@gmail.com>
2018-10-07 21:17:57 +0800

    Added "Select Case" demo

---
+++ Select_Case/Select_Case/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub lstData_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstData.SelectedIndexChanged
+        'Declare variables
+        Dim strName As String
+        Dim strFavoriteColor As String
+
+        'Get the selected name
+        strName = lstData.Items(lstData.SelectedIndex).ToString
+
+        'Use a select Case Statement to get the favourite colour of
+        'of the selected name
+
+        Select Case strName
+            Case "Bryan"
+                strFavoriteColor = "Sea Blue"
+
+            Case "Kevin"
+                strFavoriteColor = "Morning Mist"
+
+            Case "Tim"
+                strFavoriteColor = "Passionate Purple"
+
+            Case "Jay"
+                strFavoriteColor = "Red"
+        End Select
+
+        'Display the favourite colour of the selected name
+        MessageBox.Show(strName & "'s favourite color is " & strFavoriteColor, "Select Demo")
+
+        'Use Select Case statement to display a person's gender
+        Select Case strName.ToLower
+            Case "bryan", "jay", "tim"
+                MessageBox.Show("This person's gender is male.", "Select Demo")
+            Case "kevin"
+                MessageBox.Show("This person's gender is unknown.", "Select Demo")
+        End Select
+    End Sub
+End Class
---
+++ Select_Case/Select_Case/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Select_Case")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Select_Case")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d725bd86-f79e-4d4d-a70d-5490ccd1a539")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
