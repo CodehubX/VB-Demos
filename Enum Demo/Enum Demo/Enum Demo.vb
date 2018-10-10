3c7411724e4c686de04ce629fff9e838d47db76e
Karuzzio <kevinkarash@gmail.com>
2018-10-10 08:37:45 +0800

    Added Enum Demo

---
+++ Enum Demo/Enum Demo/Enum Demo.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    'DayAction Enumerator
+    Private Enum DayAction As Integer
+        Asleep = 0
+        GettingReadyForWork = 1
+        TravellingToWork = 2
+        AtWork = 3
+        AtLunch = 4
+        TravellingFromWork = 5
+        RelaxingWithFriends = 6
+        GettingReadyForBed = 7
+    End Enum
+End Class
