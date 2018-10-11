81d92fea42046e347b2d5f42d8c0e4c950ae365b
Karuzzio <kevinkarash@gmail.com>
2018-10-11 15:37:26 +0800

    Added Constants Demo

---
+++ Constants Demo/Constants Demo/Constants Demo.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    'File name constant
+    Private Const strFileName As String = "D:\Hello.txt"
+
+    Private Sub btnOne_Click(sender As Object, e As EventArgs) Handles btnOne.Click
+        'Using a constant
+        MessageBox.Show("1: " & strFileName, "Constants Demo")
+    End Sub
+
+    Private Sub btnTwo_Click(sender As Object, e As EventArgs) Handles btnTwo.Click
+        'Using the constant again
+        MessageBox.Show("2: " & strFileName, "Constants Demo")
+    End Sub
+End Class
--- Enum Demo/Enum Demo/Enum Demo.vb
+++ Enum Demo/Enum Demo/Enum Demo.vb
@@ -10,4 +10,52 @@
         RelaxingWithFriends = 6
         GettingReadyForBed = 7
     End Enum
+
+    'Declare vairable
+    Private CurrentState As DayAction
+
+    'Hour property
+    Private Property Hour() As Integer
+        Get
+            'Return the current hour displayed
+            Return dtpHour.Value.Hour
+        End Get
+        Set(value As Integer)
+            'Set the date using the hour passsed to this property
+            dtpHour.Value =
+                New Date(Now.Year, Now.Month, Now.Day, value, 0, 0)
+
+            'Determine the state
+            If value >= 6 And value < 7 Then
+                CurrentState = DayAction.GettingReadyForWork
+            ElseIf value >= 7 And value < 8 Then
+                CurrentState = DayAction.TravellingToWork
+            ElseIf value >= 8 And value < 9 Then
+                CurrentState = DayAction.AtWork
+            ElseIf value >= 9 And value < 10 Then
+                CurrentState = DayAction.AtLunch
+            ElseIf value >= 10 And value < 11 Then
+                CurrentState = DayAction.AtWork
+            ElseIf value >= 11 And value < 12 Then
+                CurrentState = DayAction.TravellingFromWork
+            ElseIf value >= 12 And value < 13 Then
+                CurrentState = DayAction.GettingReadyForBed
+            ElseIf value >= 13 And value < 14 Then
+                CurrentState = DayAction.Asleep
+            End If
+
+            'Set the display text
+            lblState.Text = "At" & value & ":00, Richard is " & CurrentState.ToString
+        End Set
+    End Property
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Set the Hour property to the current hour
+        Me.Hour = Now.Hour
+    End Sub
+
+    Private Sub dtpHour_ValueChanged(sender As Object, e As EventArgs) Handles dtpHour.ValueChanged
+        'Update the Hour property
+        Me.Hour = dtpHour.Value.Hour
+    End Sub
 End Class
